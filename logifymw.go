6d85737b724d54fa318a73cd3dad3d654dc1d67d
Eric Titcombe <eric.titcombe@sunlife.com>
2020-04-23 14:21:15 -0400

    Add LogItMoreMore which adds the status and the size to the log message.

--- logifymw.go
+++ logifymw.go
@@ -25,7 +25,38 @@ func LogItMore(mux http.Handler) http.HandlerFunc {
 		mux.ServeHTTP(w, r)
 	}
 }
+
+// LogItMoreMore adds the status and the size to the log message to what was
+// already added by LogItMore
+func LogItMoreMore(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		now := time.Now()
+		lw := loggingResponseWriter{w, 0, 0}
+		h.ServeHTTP(&lw, r)
+
+		msg := fmt.Sprintf("%-15s %-4s %-50s %s %d %d", r.RemoteAddr, r.Method, r.URL.EscapedPath()+" "+r.URL.RawQuery, r.UserAgent(), lw.status, lw.size)
+		log.Printf("%s %s", msg, time.Since(now))
+	})
+}
+
 func measureTime(msg string, start time.Time) {
 	elapsed := time.Since(start)
 	log.Printf("%s %s", msg, elapsed)
+}
+
+type loggingResponseWriter struct {
+	http.ResponseWriter
+	status int
+	size   int
+}
+
+func (w *loggingResponseWriter) Write(data []byte) (int, error) {
+	written, err := w.ResponseWriter.Write(data)
+	w.size += written
+	return written, err
+}
+
+func (w *loggingResponseWriter) WriteHeader(statusCode int) {
+	w.ResponseWriter.WriteHeader(statusCode)
+	w.status = statusCode
 }
--- logifymw_test.go
+++ logifymw_test.go
@@ -2,6 +2,7 @@ package logifymw_test
 
 import (
 	"bytes"
+	"fmt"
 	"log"
 	"net/http"
 	"net/http/httptest"
@@ -52,8 +53,29 @@ func TestLogItMore(t *testing.T) {
 	}
 }
 
+func TestLogItMoreMore(t *testing.T) {
+	var buf bytes.Buffer
+	log.SetOutput(&buf)
+
+	ts := httptest.NewServer(logifymw.LogItMoreMore(testHandler()))
+	defer ts.Close()
+
+	_, err := http.Get(ts.URL)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// We should get a string like this:
+	// 2020/04/23 14:17:04 127.0.0.1:58790 GET  /                                                  Go-http-client/1.1 200 5 0s
+	ok, err := regexp.MatchString(`^\d\d\d\d\/\d\d\/\d\d \d\d:\d\d:\d\d 127.0.0.1:\d+ GET  \/\s+ Go-http-client\/1.1 200 5 0s`, buf.String())
+	if !ok {
+		t.Error("log doesn't match", buf.String(), err)
+	}
+}
+
 func testHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		// blorf
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, "blorf")
 	}
 }
